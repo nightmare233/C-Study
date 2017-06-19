c93bc869352d55d6751e9a4959da8819a3642830
= <=>
2017-06-19 14:04:43 +0800

    0619

--- Box.cs
+++ Box.cs
@@ -4,9 +4,41 @@ namespace CodeDev
 {
     public class Box
     {
-        private String _name;
-        private string _age;
+        //构造函数
+        public Box(){
+
+        }
+
+        //原来的正常的写法：
+        private string _name;
+        private int _age;
+
+        public String Name{
+            get{
+                return _name;
+                }
+            set{
+                _name = value;
+            }
+        }
+
+        public int Age{
+            get{
+                return _age;
+            }
+            set{
+                _age = value;
+            }
+        }
 
 
+        //自动属性
+        public String Title{get;set;}
+        public string Readers{get;set;}
+
+        public string GetBoxName()
+        {
+            return this.Name+" you got it by class internal method...";
+        }
     }   
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,25 @@ namespace CodeDev
             Console.WriteLine("Frank test!");
             string box = Box("frank",32);
             Console.WriteLine(box);
+
+            //传统试试创建对象：
+            Box box2 = new Box();
+                box2.Name = "frank";
+                box2.Age = 32;
+                box2.Title = "<You are the best>";
+                box2.Readers = "3000000";
+            //初始化器
+            Box box3 = new Box(){
+                Name = "Lee",
+                Age = 23,
+                Title = "developer"
+            };
+
+           string result =  box2.GetBoxName();
+           string result2 = box3.GetBoxName();
+
+           Console.WriteLine(result);
+           Console.WriteLine(result2);
         }
 
         private static string Box(string name, int age){
